9fbe1caa066950782300ab52a3b0505608852955
Jaimish Patel <jaimish.patel95@gmail.com>
2021-07-01 12:49:18 -0400

    1

---
+++ _nuxt/0d2832e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{828:function(t,e,n){"use strict";n.r(e);var r={layout:"default",fetch:function(t){(0,t.redirect)("/transfer")}},c=n(13),component=Object(c.a)(r,(function(){var t=this.$createElement;return(this._self._c||t)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
